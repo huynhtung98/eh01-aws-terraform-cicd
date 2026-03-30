6bb54f85313762759b05d460eba06683053ddbc9
Elvin <huynhtung98@gmail.com>
2026-03-31 00:39:20 +1100

    add backend config and lock file

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-eh01-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "eh01-terraform-lock"
+    encrypt        = true
+  }
+}
