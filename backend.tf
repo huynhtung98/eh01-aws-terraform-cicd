d059ec2f491d0eaaa1207c2f64bbd975f863d9fd
Elvin <huynhtung98@gmail.com>
2026-03-31 01:01:19 +1100

    modify backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket         = "s3-eh01-terraform-state"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "eh01-terraform-lock"
-    encrypt        = true
+    required_version = ">= 1.3.0"
+
+    backend "s3" {
+        bucket         = "s3-eh01-terraform-state"
+        key            = "prod/terraform.tfstate"
+        region         = "ap-southeast-1"
+        dynamodb_table = "eh01-terraform-lock"
+        encrypt        = true
   }
 }
